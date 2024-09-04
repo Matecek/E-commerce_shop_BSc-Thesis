cb229672f3dbb5343a8f3a1b69033fb7a0a4dba9
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-09-04 15:55:07 +0200

    Stylowanie

--- front-end/src/components/FavoriteList/FavoriteList.jsx
+++ front-end/src/components/FavoriteList/FavoriteList.jsx
@@ -1,16 +1,23 @@
 import { FavoriteProduct } from "../FavoriteProduct/FavoriteProduct";
 import { CenterContent } from "../CenterContent/CenterContent";
 
+import styles from "./FavoriteList.module.css";
+
 export function FavoriteList({ products }) {
     return (
         <CenterContent>
-            <h2>Ulubione</h2>
-            <div>
-                {products.map((product) => {
-                    return (
-                        <FavoriteProduct key={product.id} product={product} />
-                    );
-                })}
+            <div className={styles.favoriteList}>
+                <h2>Ulubione</h2>
+                <div>
+                    {products.map((product) => {
+                        return (
+                            <FavoriteProduct
+                                key={product.id}
+                                product={product}
+                            />
+                        );
+                    })}
+                </div>
             </div>
         </CenterContent>
     );
--- front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
+++ front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
@@ -1,3 +1,32 @@
+import BAG_ICON from "../../assets/bag.svg";
+import REMOVE_ICON from "../../assets/remove.svg";
+
+import styles from "./FavoriteProduct.module.css";
+
 export function FavoriteProduct({ product }) {
-    return <p>{product.productName}</p>;
+    return (
+        <div className={styles.favoriteProduct}>
+            <img src={product.photos[0]} />
+            <div>
+                <h3 className={styles.brand}>
+                    {product.brand} {product.productName}
+                </h3>
+                <p>{product.pricePLN}zł</p>
+            </div>
+            <p className={styles.favoriteProductInfo}>
+                <span>Cena: </span>
+                {product.pricePLN}zł
+            </p>
+            <div>
+                <button>
+                    <img src={REMOVE_ICON} />
+                    Usuń z ulubionych
+                </button>
+                <button>
+                    <img src={BAG_ICON} />
+                    Przenieś do koszyka
+                </button>
+            </div>
+        </div>
+    );
 }
