836a7e34bc66806cd0bbae027e223ab6010e9747
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-09-05 11:37:55 +0200

    Ułożenie wszystkich komponentów ulubionych

--- front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
+++ front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
@@ -7,25 +7,27 @@ export function FavoriteProduct({ product }) {
     return (
         <div className={styles.favoriteProduct}>
             <img src={product.photos[0]} />
-            <div>
-                <h3 className={styles.brand}>
-                    {product.brand} {product.productName}
-                </h3>
-                <p>{product.pricePLN}zł</p>
-            </div>
-            <p className={styles.favoriteProductInfo}>
-                <span>Cena: </span>
-                {product.pricePLN}zł
-            </p>
-            <div>
-                <button>
-                    <img src={REMOVE_ICON} />
-                    Usuń z ulubionych
-                </button>
-                <button>
-                    <img src={BAG_ICON} />
-                    Przenieś do koszyka
-                </button>
+            <div className={styles.favoriteProductInfo}>
+                <div className={styles.brand}>
+                    <h3>
+                        {product.brand} {product.productName}
+                    </h3>
+                    <p>{product.pricePLN}zł</p>
+                </div>
+                <p className={styles.price}>
+                    <span>Cena: </span>
+                    {product.pricePLN}zł
+                </p>
+                <div className={styles.button}>
+                    <button>
+                        <img src={REMOVE_ICON} />
+                        Usuń z ulubionych
+                    </button>
+                    <button>
+                        <img src={BAG_ICON} />
+                        Przenieś do koszyka
+                    </button>
+                </div>
             </div>
         </div>
     );
