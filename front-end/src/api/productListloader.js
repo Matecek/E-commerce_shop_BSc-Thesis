7ab63a222d5f10ad7f08995b966682c1501c524a
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-10-09 15:37:54 +0200

    integracja z api

--- front-end/src/api/productListloader.js
+++ front-end/src/api/productListloader.js
@@ -1,7 +1,18 @@
+import { redirect } from "react-router-dom";
 import { BACK_END_URL, PATH_TO_ENDPOINT_MAPPING } from "../const/api";
+import { CATEGORIES } from "../const/categories";
 
 export function productListLoader({ params: { gender, category } }) {
-    return fetch(
-        `${BACK_END_URL}/products/?gender=${PATH_TO_ENDPOINT_MAPPING[gender]}&category=${category}`
+    const foundCategory = CATEGORIES.find(
+        (element) => element.path === category
     );
+    const foundGender = PATH_TO_ENDPOINT_MAPPING[gender];
+
+    if (foundCategory && foundGender) {
+        return fetch(
+            `${BACK_END_URL}/products/?gender=${foundGender}&category=${category}`
+        );
+    } else {
+        return redirect("/");
+    }
 }
--- front-end/src/views/ProductsList/ProductsList.jsx
+++ front-end/src/views/ProductsList/ProductsList.jsx
@@ -3,10 +3,11 @@ import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
 import { Products } from "../../components/Products/Products";
 import { Pagination } from "../../components/Pagination/Pagination";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export function ProductsList() {
     const products = useLoaderData();
+    const params = useParams();
 
     return (
         <FlexContainer>
