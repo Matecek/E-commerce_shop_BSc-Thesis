ed5346cac657cc6620738a4302bd83d8e75adf89
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-03-31 09:00:06 +0200

    Przeniesienie plików

--- front-end/src/components/Content/Content.jsx
+++ front-end/src/components/Content/Content.jsx
@@ -1,5 +1,3 @@
-import styles from "./Content.module.css";
-
 export function Content({ children }) {
-    return <div className={styles.content}>{children}</div>;
+    return <div>{children}</div>;
 }
